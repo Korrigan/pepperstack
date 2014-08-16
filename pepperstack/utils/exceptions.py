f22a852a85e5ddada4aa5ff0bb1021f13c0a8900
Matthieu Rosinski <korrigan@staff.42.fr>
2014-08-16 11:26:42 +0200

    added exception classes for pepperstack

---
+++ pepperstack/utils/exceptions.py
@@ -0,0 +1,29 @@
+"""
+Module containing useful exceptions for pepperstack
+
+"""
+
+class PepperstackException(Exception):
+    """
+    Base class for all pepperstack exception to make
+    catch-all easier
+
+    """
+    pass
+
+
+class DoesNotExistsException(PepperstackException):
+    """
+    An exception class raised when a model is not found
+
+    """
+    pass
+
+
+class DuplicateException(PepperstackException):
+    """
+    An exception class raised when trying to create duplicate
+    entries in database
+
+    """
+    pass
