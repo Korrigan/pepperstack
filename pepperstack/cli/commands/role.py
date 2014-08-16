1e8c5db19d64d0c269647c9c78f03ede12bb189f
Matthieu Rosinski <korrigan@staff.42.fr>
2014-08-16 11:25:29 +0200

    added role.create and role.delete commands

--- pepperstack/cli/commands/role.py
+++ pepperstack/cli/commands/role.py
@@ -4,7 +4,7 @@ This module contains all role related commands for pepperstsack CLI
 """
 
 from pepperstack.models import Role
-
+from pepperstack.utils.exceptions import DoesNotExistsException
 from pepperstack.cli.format import title, indent, pretty_print
 
 
@@ -20,7 +20,6 @@ class role_list:
         print title("Roles:")
         for r in Role.filter(**filters):
             print " - {0}".format(r.name)
-        return True
 
 
 class role_show:
@@ -34,7 +33,40 @@ class role_show:
     def __call__(self, name):
         r = Role.find(name)
         if not r:
-            return False
+            raise DoesNotExistsException("Role {0} does not exists in database"
+                                         .format(name))
         print title(r.name + ':')
         pretty_print(r.as_dict())
-        return True
+
+
+class role_create:
+    """
+    Create a role
+
+    """
+    description = "Create role <name>"
+    help_text = ""
+
+    def __call__(self, name, **data):
+        r = Role.create(name, data)
+        if not r:
+            raise DoesNotExistsException("Cannot create role {0}"
+                                         .format(name))
+        role_show()(name)
+
+
+class role_delete:
+    """
+    Delete a role
+
+    """
+    description = "Delete role <name>"
+    help_text = ""
+
+    def __call__(self, name):
+        r = Role.find(name)
+        if not r:
+            raise DoesNotExistsException("Role {0} does not exists in database"
+                                         .format(name))
+        r.delete()
+        print "Role {0} deleted".format(name)
