b44c347044e199ad1598ca6c1f4fb46601b1c198
Matthieu Rosinski <korrigan@staff.42.fr>
2014-08-18 11:15:14 +0200

    added more Model methods

---
+++ pepperstack/commands/mixins.py
@@ -0,0 +1,43 @@
+"""
+Base classes for commands implementation
+
+"""
+
+class CommandMixin(object):
+    """
+    Base class for implementing commands
+    - description will be shown when running `pepper help`
+    - help_text will be shown when running `pepper help <command>`
+    - cli_mode sets the behavior of the command, if True, the results will be
+      printed on the standard output
+
+    When using this base class please take care of the cli_mode attribute
+
+    """
+    description = "Pepperstack command"
+    help_text = "Pepperstach command long help"
+    cli_mode = False
+
+
+    def __init__(self, cli_mode=False, description=None, help_text=None):
+        self.cli_mode = cli_mode
+        if description:
+            self.description = description
+        if help_text:
+            self.help_text = help_text
+
+
+    def get_description(self):
+        """
+        A little method if needed for advanced needs
+
+        """
+        return self.description
+
+
+    def get_help_text(self):
+        """
+        A little method if needed for advanced needs
+
+        """
+        return self.help_text
